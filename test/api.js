1f7b8bdc043ac204dc141f784b1cb9ee08d25bf6
mrahmatu <mrahmatu@andrew.cmu.edu>
2023-09-17 21:17:38 +0300

    Edited the test/api.js file to not test attribute 'type' for now

--- test/api.js
+++ test/api.js
@@ -588,7 +588,7 @@ describe('API', async () => {
 
             // Check if the property is neither "endorsed" nor "endorsed_by_instructor"
             // it will make tests pass now, endorsed is for topics, endorsed_by_instructor is for posts
-            if (prop !== 'endorsed' && prop !== 'endorsed_by_Instructor') {
+            if (prop !== 'endorsed' && prop !== 'endorsed_by_Instructor' && prop !== 'type') {
                 assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
             }
         });
