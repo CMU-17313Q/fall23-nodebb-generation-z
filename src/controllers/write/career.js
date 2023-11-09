10b8a3968bcb69a40107076ba08aac3c4f1d0d80
mrahmatu <mrahmatu@andrew.cmu.edu>
2023-11-09 19:36:24 +0300

    edited the write/career.js file to send http request to python microservice to get the correct prediction

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -3,7 +3,7 @@
 const helpers = require('../helpers');
 const user = require('../../user');
 const db = require('../../database');
-
+const axios = require('axios');
 const Career = module.exports;
 
 Career.register = async (req, res) => {
@@ -20,8 +20,14 @@ Career.register = async (req, res) => {
             num_past_internships: userData.num_past_internships,
         };
 
-        userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
-
+        try {
+            const apiUrl = 'https://career-microservice-f474tbkenq-uc.a.run.app/predict';
+            const response = await axios.get(apiUrl, { params: userCareerData });
+            userCareerData.prediction = String(response.data.good_employee);
+        } catch (error) {
+            console.error('Error while making the HTTP request:', error);
+        }
+        console.log(userCareerData)
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
         res.json({});
